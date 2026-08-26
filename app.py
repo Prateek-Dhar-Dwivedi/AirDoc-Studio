59dcf847b43eccc94bb83348d174b1ba53690d26
mkp145 <prateekdhardwivedi10@gmail.com>
2026-08-26 21:22:16 +0530

    Remove unsupported ssr_mode argument from demo.launch()

--- app.py
+++ app.py
@@ -96,4 +96,4 @@ with gr.Blocks(theme=custom_theme, title="AirDoc Studio") as demo:
     )
 
 if __name__ == "__main__":
-    demo.launch(server_name="0.0.0.0", server_port=7860, ssr_mode=False)
+    demo.launch(server_name="0.0.0.0", server_port=7860)
